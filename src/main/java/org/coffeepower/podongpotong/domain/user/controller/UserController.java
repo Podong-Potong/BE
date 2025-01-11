27f3310b3a8217d441776829758847849b6e6ed0
HYEONSIK <ks06891@naver.com>
2025-01-11 19:45:02 +0900

    refactor: put에서 post로 리펙토링

--- src/main/java/org/coffeepower/podongpotong/domain/user/controller/UserController.java
+++ src/main/java/org/coffeepower/podongpotong/domain/user/controller/UserController.java
@@ -1,5 +1,7 @@
 package org.coffeepower.podongpotong.domain.user.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.coffeepower.podongpotong.domain.user.dto.RegisterReqDto;
 import org.coffeepower.podongpotong.domain.user.service.UserService;
@@ -8,14 +10,17 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/user")
+@RequestMapping("/api/user")
+@Tag(name = "User API", description = "유저 인증 관련 API")
 public class UserController {
 
     private final UserService userService;
 
     @GetMapping("/DuplicateEmailCheck")
+    @Operation(summary = "이메일 중복 확인", description = "회원가입 시, 이메일 중복 확인 API")
     public String DuplicateEmailCheck(@RequestParam("email") String email) { return userService.duplicateEmailCheck(email); }
 
-    @PutMapping("/")
+    @PostMapping("/")
+    @Operation(summary = "회원가입", description = "회원가입 API")
     public String registerUser(@RequestBody RegisterReqDto registerReqDto) { return userService.registerUser(registerReqDto); }
 }
