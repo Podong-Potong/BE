93199f25c6deccdbe650d7cb80b7e9b9beffd4b6
HYEONSIK <ks06891@naver.com>
2025-01-11 14:33:40 +0900

    init: 프로젝트 초기화
    - Spring Boot 기본 폴더 정리
    - git ignore에 .yml 파일 추가
    - build.gradle 의존성 추가

---
+++ src/main/java/org/coffeepower/podongpotong/PodongPotongApplication.java
@@ -0,0 +1,13 @@
+package org.coffeepower.podongpotong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodongPotongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PodongPotongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/coffeepower/podongpotong/PodongPotongApplicationTests.java
@@ -0,0 +1,13 @@
+package org.coffeepower.podongpotong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodongPotongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
